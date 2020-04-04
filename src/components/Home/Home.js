cd6f4ab2dbbb7c38d340b949875ac8e931deb165
JuliaNeumann <jn.julianeumann@googlemail.com>
2020-04-04 12:41:48 +0200

    change homepage styling

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,39 +8,30 @@ const data = require("./Home.json");
 function Home() {
   return (
     <LocaleContext.Consumer>
-      {({locale}) => (
+      {({ locale }) => (
         <div className="home">
-          {data.main.map((section, index) => {
-            const columnModifiers =
-              index === data.main.length - 1
-                ? "home__column--last"
-                : `home__column--${index} is-offset-${index * 4}`;
-            return (
-              <div key={section.icon} className="home__box-wrapper columns">
-                {index === 0 && (
-                  <div className="home__column home__column--intro column">
-                    <p className="home__teaser-text">{data.intro[locale]}</p>
-                  </div>
-                )}
-                <div className={`home__column ${columnModifiers} column is-4`}>
-                  <section className="box">
+          <p className="home__teaser-text">{data.intro[locale]}</p>
+          <div className="columns">
+            {data.main.map((section, index) => {
+              return (
+                <div
+                  key={section.icon}
+                  className="column is-4"
+                >
+                  <section className="home__box box">
                     <h2 className="home__title title is-4">
                       {section.title[locale]}
                     </h2>
-                    <p>{section.body[locale]}</p>
+                    <p className="home__text">{section.body[locale]}</p>
                     <div className="home__icon-wrapper">
                       <i className={`home__icon fas fa-${section.icon}`}></i>
                     </div>
                   </section>
                 </div>
-                {index === data.main.length - 1 && (
-                  <div className="home__column home__column--outro column is-8">
-                    <p className="home__teaser-text">{data.outro[locale]}</p>
-                  </div>
-                )}
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
+          <p className="home__teaser-text">{data.outro[locale]}</p>
         </div>
       )}
     </LocaleContext.Consumer>
